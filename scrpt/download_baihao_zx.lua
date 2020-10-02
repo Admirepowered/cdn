d0d9fbdb6d4726033cc7199644f5e07af439a4a4
Admirepowered <43035036+Admirepowered@users.noreply.github.com>
2020-10-03 00:32:14 +0800

    Add files via upload

---
+++ scrpt/download_baihao_zx.lua
@@ -0,0 +1,85 @@
+﻿local curl = require "lcurl.safe"
+local json = require "cjson.safe"
+
+
+script_info = {
+	["title"] = "白号通道[自选]",
+	["version"] = "0.0.3",
+	["description"] = "白号通道(推荐64线程)",
+	["color"] = "#FF55CC",
+}
+     
+function onInitTask(task, user, file)
+    if task:getType() ~= TASK_TYPE_BAIDU then
+		return false
+    end
+    if user == nil then
+        task:setError(-1, "用户未登录")
+		return true
+	end	
+	local appid = 250528
+	local BDUSS = user:getBDUSS()
+	local BDS = user:getBDStoken()
+	local Cookie = user:getCookie()
+	local header = { "User-Agent: netdisk;P2SP;2.2.60.26" }
+	table.insert(header, "Cookie: BDUSS="..BDUSS.."SignText")
+	local url = "http://127.0.0.1:8989/api/gd3?path="..pd.urlEncode(file.path)
+    local data = ""
+	local c = curl.easy{
+		url = url,
+		followlocation = 1,
+		httpheader = header,
+		timeout = 15,
+		proxy = pd.getProxy(),
+		writefunction = function(buffer)
+			data = data .. buffer
+			return #buffer
+		end,
+	}
+	
+	
+	local _, e = c:perform()
+    c:close()
+    if e then
+        return false
+    end
+	
+	local j = json.decode(data)
+	if j == nil then
+        return false
+    end
+	
+	local message = {}
+    local downloadURL = ""
+    for i, w in ipairs(j.urls) do
+	    downloadURL = w.url
+		local d_start = string.find(downloadURL, "//") + 2
+        local d_end = string.find(downloadURL, "%.") - 1
+		downloadURL = string.sub(downloadURL, d_start, d_end)
+        table.insert(message, downloadURL)
+    end
+	local num = pd.choice(message, 1, "选择下载接口")
+    downloadURL = j.urls[num].url
+	--local downloadURL = data
+	
+
+	--downloadURL = "https"..string.sub(downloadURL, 0, string.len(downloadURL)-4)
+	
+	pd.logInfo(downloadURL)
+	task:setUris(downloadURL)
+    task:setOptions("user-agent", "netdisk;2.2.51.6;netdisk;10.0.63;PC;android-android;QTP/1.0.32.2")
+    task:setOptions("header", "Range:bytes=0-0")
+	--if file.size >=8192 then 
+	--	task:setOptions("header", "Range:bytes=4096-8191")
+	--end
+	if user:isSVIP() then
+		task:setIcon("icon/svip.png", "Powered By-Admire")
+	end
+	task:setIcon("icon/tips_warn.png", "XXX")
+    task:setOptions("piece-length", "1M")
+	--task:setOptions("max-connection-per-server", "64")
+	--task:setOptions("split", "64")
+    task:setOptions("allow-piece-length-change", "true")
+    task:setOptions("enable-http-pipelining", "true")
+    return true
+end
